d2158b713b1c74eb8af49536aac9ba11894a08c5
skrtdev <64538010+skrtdev@users.noreply.github.com>
2021-03-29 16:24:58 +0200

    fix custom range()

--- src/range.php
+++ src/range.php
@@ -3,7 +3,7 @@
 namespace skrtdev\async;
 
 function range (int $start, int $end, int $step = 1): \Generator {
-    for ($i=$start; $i < $end; $i++) {
+    for ($i = $start; $i < $end; $i += $step) {
         yield $i;
     }
 }
