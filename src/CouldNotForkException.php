4ff620bc3b6d1fb3c083d9744e32ee28697cce0b
skrtdev <64538010+skrtdev@users.noreply.github.com>
2021-05-12 10:48:12 +0200

    fix

--- src/CouldNotForkException.php
+++ src/CouldNotForkException.php
@@ -3,6 +3,6 @@ declare(strict_types=1);
 
 namespace skrtdev\async;
 
-class CouldNotForkException extends Exception {}
+class CouldNotForkException extends \Exception {}
 
 ?>
--- src/MissingExtensionException.php
+++ src/MissingExtensionException.php
@@ -3,6 +3,6 @@ declare(strict_types=1);
 
 namespace skrtdev\async;
 
-class MissingExtensionException extends Exception {}
+class MissingExtensionException extends \Exception {}
 
 ?>
